1a98c6521859f1b627c9a31d0b0d8f823511ca81
Tristan Lifshitz-Nielsen <tristanlifn@gmail.com>
2023-02-09 11:19:09 +0100

    main lav skærm

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+# lav en skærm (1200X600)
+
 import pygame
+from sys import exit
+import math
+pygame.init()
+
+size = width, height = 1200, 600
+
+origin = width/2, 0
+
+white = 255, 255, 255
+black = 0,0,0
+
+screen = pygame.display.set_mode(size)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(black)
+    pygame.display.flip()
+
+pygame.quit()
+quit()
