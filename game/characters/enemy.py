3d21d5f3128859e8e584c7cf7ec199ae9b13beef
Lucas Christensen <lucas@rosenvold.tech>
2023-02-23 00:48:09 +0100

    Updated speed and added ghost

--- game/characters/enemy.py
+++ game/characters/enemy.py
@@ -5,17 +5,25 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self, screen_size):
         super().__init__()
 
-        # Load and scale enemy image
-        self.image = pygame.image.load(os.path.join('game', 'sprites', 'enemy.jpg')).convert_alpha()
-        self.rect = self.image.get_rect()
+        # Load enemy spritesheet
+        spritesheet = pygame.image.load(os.path.join('game', 'sprites', 'ghost', 'ghost.png')).convert_alpha()
 
         # Set enemy properties
-        self.speed = 2
+        self.speed = 3
         self.position = pygame.math.Vector2(screen_size[0] / 2, screen_size[1] / 2)
 
+        # Define the sprite dimensions and animation frames
+        frame_width = 60
+        frame_height = 60
+        num_frames = 11
+        self.frames = [spritesheet.subsurface(pygame.Rect((i * frame_width, 0), (frame_width, frame_height)))
+                       for i in range(num_frames)]
+        self.current_frame = 0
+
         # Scale enemy to 4% of screen size
-        scale_factor = int(screen_size[0] * 0.04) / self.rect.width
-        self.image = pygame.transform.rotozoom(self.image, 0, scale_factor)
+        scale_factor = int(screen_size[0] * 0.04) / frame_width
+        self.frames = [pygame.transform.rotozoom(frame, 0, scale_factor) for frame in self.frames]
+        self.image = self.frames[self.current_frame]
         self.rect = self.image.get_rect()
 
     def update(self, player_position):
@@ -25,3 +33,7 @@ class Enemy(pygame.sprite.Sprite):
             direction.scale_to_length(self.speed)
         self.position += direction
         self.rect.center = self.position
+
+        # Update the enemy sprite animation
+        self.current_frame = (self.current_frame + 1) % len(self.frames)
+        self.image = self.frames[self.current_frame]
