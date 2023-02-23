3fcb9203aa837b3c8854a85b131e734c8fc5975b
Lucas Christensen <lucas@rosenvold.tech>
2023-02-23 01:14:53 +0100

    Added background to the game

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import pygame
 from game.characters.player import Player
 from game.characters.enemy import Enemy
-#from pygame.locals import *
+from pygame.locals import *
 
 # Initialize Pygame
 pygame.init()
@@ -16,19 +16,14 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
 # Set the window title
 pygame.display.set_caption("The Adventures of Young Gravy")
 
-# Set the background color to white
-background_color = (255, 255, 255)
+# Load the background image
+background_image = pygame.image.load(os.path.join('game', 'baggrunde', 'Kirkegaard.png')).convert()
+background_image = pygame.transform.scale(background_image, WINDOW_SIZE)
 
 # Create the player and enemy objects and the sprite group
 player = Player(WINDOW_SIZE)
 enemy = Enemy(WINDOW_SIZE)
 all_sprites = pygame.sprite.Group(player, enemy)
-
-# Set up the clock to limit the game to 60 fps
-clock = pygame.time.Clock()
-
-# Set up the font to display the FPS counter
-font = pygame.font.Font(None, 36)
 
 # Exit function
 def exitfn(keys):
@@ -37,18 +32,16 @@ def exitfn(keys):
         quit()
 
 # Main game loop
+clock = pygame.time.Clock()
 running = True
 while running:
-    # Limit the game to 60 fps
-    clock.tick(60)
-
     # Handle events
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == QUIT:
             running = False
 
-    # Fill the screen with the background color
-    screen.fill(background_color)
+    # Blit the background image onto the screen
+    screen.blit(background_image, (0, 0))
 
     # Draw the sprites
     all_sprites.draw(screen)
@@ -61,12 +54,11 @@ while running:
     # Run exitfn function
     exitfn(keys)
 
-    # Draw the FPS counter in the top left corner of the screen
-    fps_text = font.render("FPS: {:.2f}".format(clock.get_fps()), True, (0, 0, 0))
-    screen.blit(fps_text, (10, 10))
-
     # Update the screen
     pygame.display.flip()
+
+    # Limit the frame rate to 60 fps
+    clock.tick(60)
 
 # Quit Pygame
 pygame.quit()
