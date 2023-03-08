e600836faefb7edcb77c2ecef01361e2bf40a926
Freja Elgin Holst <frejaelginholst@gmail.com>
2023-03-08 20:56:23 +0100

    Demon Script Creation

---
+++ game/characters/Demon.py
@@ -0,0 +1,11 @@
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, screen_size):
+        super().__init__()
+
+        # Load and scale Demon image
+        self.image = pygame.image.load(os.path.join('game', 'sprites', 'Demon.png')).convert_alpha()
+        self.rect = self.image.get_rect()
+                
+        scale_factor = int(screen_size[0] * 4) / self.rect.width
+        self.image = pygame.transform.rotozoom(self.image, 0, scale_factor)
+        self.rect = self.image.get_rect()
