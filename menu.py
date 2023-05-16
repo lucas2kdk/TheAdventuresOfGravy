69587652f19da8c0d361d76392b16d9f936e535f
Lucas Christensen <lucas@rosenvold.tech>
2023-05-16 13:33:03 +0200

    Updated menu functionality and design

--- menu.py
+++ menu.py
@@ -1,11 +1,24 @@
 import pygame
-import os
-from main import play
 from game.characters.player import Player
 from game.characters.enemy import Enemy
+from main import play
+
+class Button:
+    def __init__(self, image, x, y):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def is_over(self, pos):
+        return self.rect.collidepoint(pos)
 
 # Initialize Pygame
 pygame.init()
+
+# Background audio
 pygame.mixer.init()
 pygame.mixer.music.load("game/sounds/Main_theme.wav")
 pygame.mixer.music.set_volume(0.3)
@@ -16,78 +29,51 @@ info = pygame.display.Info()
 screen = pygame.display.set_mode((info.current_w, info.current_h), pygame.FULLSCREEN)
 
 # Set background
-background_image = pygame.image.load(os.path.join('game', 'baggrunde', 'Bo.png')).convert()
+background_image = pygame.image.load("game/baggrunde/Bo.png").convert()
 picture = pygame.transform.scale(background_image, (screen.get_width(), screen.get_height()))
 screen.blit(picture, (0, 0))
 
 # Set up the font
-font = pygame.font.SysFont(None, int(screen.get_height() * 0.045))
+font = pygame.font.Font('game/fonts/treasure.ttf', int(screen.get_height() * 0.045))
 
-def create_button(position, size, color, text, text_color,type):
-    # Create a new surface for the button
-    button = pygame.Surface(size)
-    # Fill the button with the specified color
-    button.fill(color)
-    # Render the button text and center it on the button surface
-    text_surf = font.render(text, True, text_color)
-    text_rect = text_surf.get_rect(center=button.get_rect().center)
-    textsurf = str(text_surf)
-
-    if type == "PLAY":
-        buttonImage = pygame.image.load(os.path.join('game', 'sprites', 'knap.png'))
-        buttonImage = pygame.transform.scale(buttonImage, (160, 80))
-        buttonImage = buttonImage.convert()
-        button.blit(buttonImage, button.get_rect())
-
-    elif type == "EXIT":
-        buttonImage = pygame.image.load(os.path.join('game', 'sprites', 'Exit Knap.png'))
-        #buttonImage = pygame.transform.scale(buttonImage, (57, 26))
-        buttonImage = pygame.transform.scale(buttonImage, (160, 80))
-        buttonImage = buttonImage.convert()
-        button.blit(buttonImage, button.get_rect())
-
-    button.blit(text_surf, text_rect)
-    button_rect = button.get_rect(center=position)
-    return button, button_rect
+# Set up the title
+title = "The Adventures of Gravy"
+title_surface = font.render(title, True, (255,255,255))
+title_rect = title_surface.get_rect(center=(info.current_w/2, info.current_h/4)) 
+screen.blit(title_surface, title_rect)
 
 # Set up the buttons
-button_size = (int(screen.get_width() * 0.104), int(screen.get_height() * 0.093))
-button_y = screen.get_height() // 2 - button_size[1] // 2
-button_positions = [
-    (int(screen.get_width() * 0.26), button_y),
-    (int(screen.get_width() * 0.74), button_y),
-]
-buttons = [
-    {"id": 1, "color": (255, 255, 255), "text": "star0t", "text_color": (0, 0, 0),"type":"PLAY"},
-    {"id": 2, "color": (255, 0, 0), "text": "Exit", "text_color": (255, 255, 255),"type":"EXIT"},
-]
-button_surfaces = []
-for i, button in enumerate(buttons):
-    button_surface, button_rect = create_button(button_positions[i], button_size, button["color"], "", button["text_color"],button["type"]) #button["text"]
-    button["rect"] = button_rect
-    button_surfaces.append(button_surface)
+button_size = (200, 100)  # Adjust button size as needed
+start_image = pygame.transform.scale(pygame.image.load("game/graphics/start.png"), button_size)  
+quit_image = pygame.transform.scale(pygame.image.load("game/graphics/stop.png"), button_size)  
+start_button = Button(start_image, info.current_w/2, info.current_h/2)
+quit_button = Button(quit_image, info.current_w/2, info.current_h/2 + button_size[1] + 20)  # The "+ 20" gives some vertical space between the buttons
 
 while True:
     # Handle events
     clock = pygame.time.Clock()
     for event in pygame.event.get():
+        pos = pygame.mouse.get_pos()
+
         if event.type == pygame.QUIT:
             # Quit Pygame and exit the program
             pygame.quit()
             quit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Check if the mouse button was pressed over any of the buttons
-            for button in buttons:
-                if button["rect"].collidepoint(event.pos) and button["id"] == 1:
-                    play(Player, Enemy)
-                elif button["rect"].collidepoint(event.pos) and button["id"] == 2:
-                    pygame.quit()
-                    quit()
 
-    clock.tick(5)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if start_button.is_over(pos):
+                play(Player, Enemy)  # Assuming 'play' starts the game
+            if quit_button.is_over(pos):
+                pygame.quit()
+                quit()
 
-    # Draw the buttons to the screen
-    for i, button_surface in enumerate(button_surfaces):
-        screen.blit(button_surface, buttons[i]["rect"])
+    # Redraw the screen and buttons
+    screen.blit(picture, (0, 0))
+    screen.blit(title_surface, title_rect)
+    start_button.draw(screen)
+    quit_button.draw(screen)
+
+    clock.tick(60)
+
     # Update the display
     pygame.display.update()
---
+++ old_menu.py
@@ -0,0 +1,93 @@
+import pygame
+import os
+from main import play
+from game.characters.player import Player
+from game.characters.enemy import Enemy
+
+# Initialize Pygame
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("game/sounds/Main_theme.wav")
+pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.play(0,0)
+
+# Set up the window
+info = pygame.display.Info()
+screen = pygame.display.set_mode((info.current_w, info.current_h), pygame.FULLSCREEN)
+
+# Set background
+background_image = pygame.image.load(os.path.join('game', 'baggrunde', 'Bo.png')).convert()
+picture = pygame.transform.scale(background_image, (screen.get_width(), screen.get_height()))
+screen.blit(picture, (0, 0))
+
+# Set up the font
+font = pygame.font.SysFont(None, int(screen.get_height() * 0.045))
+
+def create_button(position, size, color, text, text_color,type):
+    # Create a new surface for the button
+    button = pygame.Surface(size)
+    # Fill the button with the specified color
+    button.fill(color)
+    # Render the button text and center it on the button surface
+    text_surf = font.render(text, True, text_color)
+    text_rect = text_surf.get_rect(center=button.get_rect().center)
+    textsurf = str(text_surf)
+
+    if type == "PLAY":
+        buttonImage = pygame.image.load(os.path.join('game', 'sprites', 'knap.png'))
+        buttonImage = pygame.transform.scale(buttonImage, (160, 80))
+        buttonImage = buttonImage.convert()
+        button.blit(buttonImage, button.get_rect())
+
+    elif type == "EXIT":
+        buttonImage = pygame.image.load(os.path.join('game', 'sprites', 'Exit Knap.png'))
+        #buttonImage = pygame.transform.scale(buttonImage, (57, 26))
+        buttonImage = pygame.transform.scale(buttonImage, (160, 80))
+        buttonImage = buttonImage.convert()
+        button.blit(buttonImage, button.get_rect())
+
+    button.blit(text_surf, text_rect)
+    button_rect = button.get_rect(center=position)
+    return button, button_rect
+
+# Set up the buttons
+button_size = (int(screen.get_width() * 0.104), int(screen.get_height() * 0.093))
+button_y = screen.get_height() // 2 - button_size[1] // 2
+button_positions = [
+    (int(screen.get_width() * 0.26), button_y),
+    (int(screen.get_width() * 0.74), button_y),
+]
+buttons = [
+    {"id": 1, "color": (255, 255, 255), "text": "star0t", "text_color": (0, 0, 0),"type":"PLAY"},
+    {"id": 2, "color": (255, 0, 0), "text": "Exit", "text_color": (255, 255, 255),"type":"EXIT"},
+]
+button_surfaces = []
+for i, button in enumerate(buttons):
+    button_surface, button_rect = create_button(button_positions[i], button_size, button["color"], "", button["text_color"],button["type"]) #button["text"]
+    button["rect"] = button_rect
+    button_surfaces.append(button_surface)
+
+while True:
+    # Handle events
+    clock = pygame.time.Clock()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # Quit Pygame and exit the program
+            pygame.quit()
+            quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Check if the mouse button was pressed over any of the buttons
+            for button in buttons:
+                if button["rect"].collidepoint(event.pos) and button["id"] == 1:
+                    play(Player, Enemy)
+                elif button["rect"].collidepoint(event.pos) and button["id"] == 2:
+                    pygame.quit()
+                    quit()
+
+    clock.tick(5)
+
+    # Draw the buttons to the screen
+    for i, button_surface in enumerate(button_surfaces):
+        screen.blit(button_surface, buttons[i]["rect"])
+    # Update the display
+    pygame.display.update()
